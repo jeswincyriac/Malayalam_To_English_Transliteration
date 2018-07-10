bd0b88c581b106560e12ff4d089ab7cb2d907f39
Jeswin <jeswincyriac.k@gmail.com>
2018-07-10 16:34:37 +0530

    readytopush

--- splitting programs/hi.py
+++
@@ -1,8 +0,0 @@
-import codecs
-fileeng = codecs.open("final.txt","r",encoding="utf-8")
-out = codecs.open("english.txt","w",encoding="utf-8")
-engword=fileeng.readline()
-while engword != "" :
-      out.write(engword)
-      engword=fileeng.readline()
-      engword=fileeng.readline()
