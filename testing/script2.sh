f5d9c89c1757e68c92198849620cf000af5de479
Jeswin <jeswincyriac.k@gmail.com>
2018-10-17 11:17:26 +0530

    modifying

--- testing/script2.sh
+++ testing/script2.sh
@@ -4,6 +4,7 @@ cd ../CRF++-0.57
 #after
 awk '{print($1" "$2)}' test.txt >test1.txt
 crf_test -m model test1.txt >tempoutput.txt
+rm test1.txt
 cp tempoutput.txt ../testing/
 cd ../testing
 #rm tempoutput.txt
@@ -14,6 +15,5 @@ cat output.txt
 rm output2.txt
 rm tempoutput.txt
 rm output1.txt
-rm output.txt
+#rm output.txt
 rm tempm.txt
-rm test1.txt
